19d4d72879ca03fc5bfb34bdb75d54dc4a06c233
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2023-05-30 21:05:19 -0400

    feat: add base compile

--- compile.bash
+++ compile.bash
@@ -1,10 +1,16 @@
-echo "ProjectID: "
-read ProjectID
+echo "Projects:"
+echo "- HedgeMaze"
+echo "- SurvivalGame"
+echo ""
+
+read -p "ProjectID: " ProjectID
 
 while true
 do
-	(
-		cd "./$ProjectID" && ~/JMC.bin compile;
-	)
-	read -p "Press Enter to Compile"
+	# (
+	# 	cd "./$ProjectID" && ~/JMC.bin compile
+	# )
+	# read -p "Press Enter to Compile"
+	sleep 3
+	echo 'hi'
 done
